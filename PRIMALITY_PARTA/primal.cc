00067bf607cbf1bf820485552ba2980b5740b217
Sh0g0-1758 <shouryagoel10000@gmail.com>
2025-01-03 20:29:26 +0530

    fix: better test and rand

--- PRIMALITY_PARTA/primal.cc
+++ PRIMALITY_PARTA/primal.cc
@@ -1,44 +1,44 @@
-#include<stdio.h>
-#include<map>
-#include<random>
+#include <stdio.h>
+#include <random>
 #include "primal.decl.h"
 
 int getRandom(int min, int max) {
-    std::random_device rd;
-    std::mt19937_64 gen(rd());
+    static int counter = 0;
+    std::mt19937_64 gen(42 + counter++);
     std::uniform_int_distribution<int> dis(min, max);
     return dis(gen);
 }
 
 class start : public CBase_start {
 private:
-std::map<int, bool> primes;
-int tot;
-int curr;
+    int* check;
+    int* nums;
+    int tot;
+    int curr;
+    double begin = 0;
+    double end = 0;
 public:
     start(CkArgMsg* m) : curr(0) {
         if(m -> argc != 2) {
-            ckout << "Usage: " << m -> argv[0] << " <n>" << endl;
+            ckout << "Usage: " << m -> argv[0] << " <n_primes>" << endl;
         }
-        tot = atoi(m -> argv[1]);
+        int n = atoi(m -> argv[1]);
+        nums = (int*)malloc(n * sizeof(int));
+        check = (int*)calloc(n, sizeof(int));
+        tot = n;
+        begin = CkTimer();
         for(int i = 0; i < tot; i++) {
-            int temp = getRandom(1, 100000000);
-            primes[temp] = false;
-            CProxy_test offload = CProxy_test::ckNew(temp, thisProxy);
+            nums[i] = getRandom(100000000, 1000000000);
+            CProxy_test offload = CProxy_test::ckNew(nums[i], i, thisProxy);
         }
     }
 
-    void result(int a, bool b) {
-        primes[a] = b;
+    void result(int index, bool b) {
+        check[index] = b;
         curr++;
         if(curr == tot) {
-            for(auto it = primes.begin(); it != primes.end(); it++) {
-                if(it -> second) {
-                    ckout << it -> first << " is prime" << endl;
-                } else {
-                    ckout << it -> first << " is not prime" << endl;
-                }
-            }
+            end = CkTimer();
+            ckout << "Time: " << end - begin << " seconds" << endl;
             CkExit();
         }
     }
@@ -46,25 +46,26 @@ public:
 
 class test : public CBase_test {
 public:
-    test(int n, CProxy_start parent) {
+    test(int n, int index, CProxy_start parent) {
+        bool b;
         if(n <= 1) {
-            parent.result(n, false);
+            b = false;
         } else {
             if(n % 2 == 0) {
-                parent.result(n, false);
+                b = n == 2 ? true : false;
             } else {
                 bool prime = true;
-                for(int i = 3; i <= (n / 2); i+=2) {
+                for(int i = 3; i * i <= n; i+=2) {
                     if(n % i == 0) {
                         prime = false;
                         break;
                     }
                 }
-                parent.result(n, prime);
+                b = prime;
             }
         }
+        parent.result(index, b);
     }
 };
-
 
 #include "primal.def.h"
