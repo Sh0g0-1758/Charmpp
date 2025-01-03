8911901f555237a49a3d05899350ff41efc9589e
Sh0g0-1758 <shouryagoel10000@gmail.com>
2025-01-03 14:55:52 +0530

    fix: get grain_size from cmd

--- PRIMALITY_PARTB/primal.cc
+++ PRIMALITY_PARTB/primal.cc
@@ -2,11 +2,8 @@
 #include <random>
 #include "primal.decl.h"
 
-#define GRAIN_SIZE 20000
-
 int getRandom(int min, int max) {
-    std::random_device rd;
-    std::mt19937_64 gen(rd());
+    std::mt19937_64 gen(42);
     std::uniform_int_distribution<int> dis(min, max);
     return dis(gen);
 }
@@ -19,12 +16,14 @@ private:
     int curr;
     double begin = 0;
     double end = 0;
+    int GRAIN_SIZE;
 public:
     start(CkArgMsg* m) : curr(0) {
         if(m -> argc != 2) {
-            ckout << "Usage: " << m -> argv[0] << " <n>" << endl;
+            ckout << "Usage: " << m -> argv[0] << " <n_primes>" << " <GRAIN_SIZE>" << endl;
         }
         int n = atoi(m -> argv[1]);
+        GRAIN_SIZE = atoi(m -> argv[2]);
         nums = (int*)malloc(n * sizeof(int));
         check = (int*)calloc(n, sizeof(int));
         tot = n / GRAIN_SIZE;
@@ -35,11 +34,11 @@ public:
                 n_arr[j] = getRandom(1, 100000000);
                 nums[i * GRAIN_SIZE + j] = n_arr[j];
             }
-            CProxy_test offload = CProxy_test::ckNew(n_arr, i, thisProxy);
+            CProxy_test offload = CProxy_test::ckNew(n_arr, GRAIN_SIZE, i, thisProxy);
         }
     }
 
-    void result(int index, bool b[GRAIN_SIZE]) {
+    void result(int index, bool b[], int GRAIN_SIZE) {
         for(int i = 0; i < GRAIN_SIZE; i++) {
             check[index * GRAIN_SIZE + i] = b[i];
         }
@@ -54,7 +53,7 @@ public:
 
 class test : public CBase_test {
 public:
-    test(int n_arr[GRAIN_SIZE], int index, CProxy_start parent) {
+    test(int n_arr[], int GRAIN_SIZE, int index, CProxy_start parent) {
         bool b_arr[GRAIN_SIZE];
         for(int j = 0; j < GRAIN_SIZE; j++) {
             int n = n_arr[j];
@@ -75,9 +74,8 @@ public:
                 }
             }
         }
-        parent.result(index, b_arr);
+        parent.result(index, b_arr, GRAIN_SIZE);
     }
 };
-
 
 #include "primal.def.h"
--- PRIMALITY_PARTB/run.sh
+++ PRIMALITY_PARTB/run.sh
@@ -1,3 +1,3 @@
 ./../../charm-v7.0.0/bin/charmc primal.ci
 ./../../charm-v7.0.0/bin/charmc -o primal  primal.cc
-./charmrun +p20 ./primal 20000
+./charmrun +p20 ./primal 20000 100
